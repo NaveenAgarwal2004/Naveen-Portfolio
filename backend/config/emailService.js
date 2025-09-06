72bf778940469eca2297b73fd09800cf6e2627e6
NaveenAgarwal2004 <naveenagarwal7624@gmail.com>
2025-09-06 21:33:11 +0530

    Update emailService.js

--- backend/config/emailService.js
+++ backend/config/emailService.js
@@ -15,7 +15,7 @@ class EmailService {
    * Initialize Nodemailer transporter with Gmail SMTP
    */
   initializeTransporter() {
-    this.transporter = nodemailer.createTransporter({
+    this.transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER,
